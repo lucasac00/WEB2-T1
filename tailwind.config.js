88250aaf71db5c9b24dc0b8995d01f17a3eb9c7d
lucasac00 <luc.fast2004@gmail.com>
2024-12-15 22:01:12 -0300

    Initial commit

---
+++ tailwind.config.js
@@ -0,0 +1,8 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ['./*.html', './src/**/*.js'], // Update paths based on your project structure
+  theme: {
+    extend: {},
+  },
+  plugins: [],
+};

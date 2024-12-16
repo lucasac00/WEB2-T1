c815b254f91ecf9ae60f35c136a6ad28b5123de4
lucasac00 <luc.fast2004@gmail.com>
2024-12-15 23:01:46 -0300

    landing page v1

--- tailwind.config.js
+++ tailwind.config.js
@@ -3,6 +3,10 @@ module.exports = {
   content: ['./*.html', './src/**/*.js'], // Update paths based on your project structure
   theme: {
     extend: {},
+    colors: {
+      'blue-primary': '#3B94FC',
+      'beige-primary': '#FFE8C9'
+    }
   },
   plugins: [],
 };

a69628410512cae63816d215989b71bb3639b0a1
PedroVicente <pedrovicente@estudante.ufscar.br>
2024-12-16 13:24:59 -0300

    add: rascunho da tela de login

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,11 +2,12 @@
 module.exports = {
   content: ['./*.html', './src/**/*.js'], // Update paths based on your project structure
   theme: {
-    extend: {},
-    colors: {
-      'blue-primary': '#3B94FC',
-      'beige-primary': '#FFE8C9'
-    }
+    extend: {
+      colors: {
+        'blue-primary': '#3B94FC',
+        'beige-primary': '#FFE8C9'
+      }
+    },
   },
   plugins: [],
 };

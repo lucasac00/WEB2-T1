549e2b42757d12a91ffac6a98debbe87c58e7b82
Zephrysz <will.tsuyoshi@gmail.com>
2024-12-19 12:53:20 -0300

    feat: agora está responsável, e alguns fix e minor changes

--- tailwind.config.js
+++ tailwind.config.js
@@ -3,6 +3,9 @@ module.exports = {
   content: ['./*.html', './src/**/*.js'], // Update paths based on your project structure
   theme: {
     extend: {
+      height: {
+        'screen-minus-header': 'calc(100vh - 4rem)',  // Custom utility for header height
+      },
       colors: {
         'blue-primary': '#3B94FC',
         'beige-primary': '#FFE8C9'
